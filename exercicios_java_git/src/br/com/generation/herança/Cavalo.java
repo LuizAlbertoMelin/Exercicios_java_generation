8e04c92b60a2911eabac633efd5ab981f364509e
Luiz Alberto Melin <luizmelin34@gmail.com>
2021-12-15 12:30:05 -0300

    exercicios dia 15/12

---
+++ exercicios_java_git/src/br/com/generation/herança/Cavalo.java
@@ -0,0 +1,11 @@
+package br.com.generation.herança;
+
+public class Cavalo extends animal{
+	void emitir() {
+		System.out.println("POCOTO POCOTO POCOTO IIIRRRRÍ, RILICHIN	");
+	}
+	void correr() {
+		System.out.println("O cavalo está correndo, pulando obstaculos");
+	}
+		
+}
---
+++ exercicios_java_git/src/br/com/generation/herança/Preguica.java
@@ -0,0 +1,10 @@
+package br.com.generation.herança;
+
+public class Preguica extends animal{
+	void emitir() {
+		System.out.println("ZZZZZZZZZZZZ, A MIMIR");
+	}
+	void subir() {
+		System.out.println("A preguiça está subindo a arvore, buscando folhas verdes");
+	}
+}
